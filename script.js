7a14ff2c0b202d62d561cdc9609536987c430aab
Jonas Schumacher <jonas.merlin.schumacher@mailbox.org>
2017-11-04 18:51:24 +0100

    second

--- script.js
+++ script.js
@@ -1,35 +1,156 @@
-var app = new Vue({
+var selectedRoom = 1;
+var userId = 0;
+
+var roomList = new Vue({
   el: '#rooms',
   data: {
-    message: 'Hello Vue!'
+    rooms: 'Hello Vue!',
+    roomName: ''
   },
   methods: {
     createRoom: function () {
-		$.ajax({
-		   url: 'http://localhost:5000/rooms',
-		   data: {
-		      name: 'test'
-		   },
-		   error: function(e) {
-		   		console.log("ERROR:")
-		   		console.log(e);
-		   },
-		   dataType: 'json',
-		   type: 'POST'
-		});
+    	if (this.roomName != '' && getNamesFromObjectArray(this.rooms, 'name').indexOf(this.roomName) == -1) {
+			$.ajax({
+			   url: 'http://localhost:5000/rooms',
+			   data: {
+			      name: this.roomName
+			   },
+			   error: function(e) {
+			   		console.log("ERROR:")
+			   		console.log(e);
+			   },
+			   success: function() {
+				console.log('Room created.');
+				updateRoomList();
+			   },
+			   dataType: 'json',
+			   type: 'POST'
+			});
+    	}
     }
+  }
+});
+
+var updateRoomList = function() {
+	$.ajax({
+		url: 'http://localhost:5000/rooms',
+		type: 'GET',
+		dataType: 'json',
+		success: function(res) {
+	  		roomList.rooms = res.data.rooms;
+			console.log(roomList.rooms);
+		}
+	});
+};
+
+updateRoomList();
+
+var userList = new Vue({
+  el: '#users',
+  data: {
+    users: 'Hello Vue!'
+  }
+});
+
+var updateUserList = function() {
+	$.ajax({
+		url: 'http://localhost:5000/users/' + selectedRoom,
+		type: 'GET',
+		dataType: 'json',
+		success: function(res) {
+			console.log(res);
+	  		userList.users = res.data.users;
+		}
+	});
+};
+
+var getNamesFromObjectArray = function(array, attribute) {
+	var attributeArray = [];
+	for (entry of array) {
+  		attributeArray.push(entry[attribute]);
+	}
+	return attributeArray;
+}
+
+updateUserList();
+
+Vue.component('roomlist-room', {
+  props: ['id', 'name'],
+  template: '<li v-on:click="changeRoom">{{ name }}</li>',
+  methods: {
+  	changeRoom: function() {
+  		selectedRoom = this.id;
+  		leaveRoom();
+  		joinRoom();
+  	}
   }
 })
 
-function reqListener () {
-  console.log(this.responseText);
-  var res = JSON.parse(this.responseText)
-  console.log(res);
-  app.message = res.data;
+var leaveRoom = function() {
+	$.ajax({
+		url: 'http://localhost:5000/rooms/' + selectedRoom + '/users',
+		type: 'DELETE',
+		dataType: 'json',
+		data: {
+			user_id: userId
+		},
+		success: function(res) {
+			console.log(res);
+	  		updateMessages();
+		}
+	});
 }
 
-var oReq = new XMLHttpRequest();
-oReq.overrideMimeType("application/json");
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "http://localhost:5000/rooms");
-oReq.send();
+var joinRoom = function() {
+	$.ajax({
+		url: 'http://localhost:5000/rooms/' + selectedRoom + '/users',
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			user_id: userId
+		},
+		success: function(res) {
+	  		updateMessages();
+		}
+	});	
+}
+
+var messages = [];
+
+var messages = new Vue({
+	el: '#messages',
+	data: {
+		messages: []
+	}
+})
+
+var updateMessages = function() {
+	$.ajax({
+		url: 'http://localhost:5000/chats/' + selectedRoom,
+		type: 'GET',
+		dataType: 'json',
+		success: function(res) {
+			console.log(res);
+	  		messages.messages = res.data.messages;
+		}
+	});
+}
+
+var createUser = function() {
+	$.ajax({
+		url: 'http://localhost:5000/users',
+		type: 'POST',
+		data: {
+			name: 'jonas'
+		},
+		dataType: 'json',
+		success: function(res) {
+			console.log(res);
+			userId = res.id;
+		}
+	});
+}
+
+createUser();
+
+updateMessages();
